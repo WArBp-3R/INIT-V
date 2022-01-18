d70092bd24125ba7c3eaa363d684077926ccc345
Deniz İmge Avcı <denizimgeavci@gmail.com>
2022-01-18 11:40:35 +0100

    Started working on the Calculator class, coded first structure.

--- init-v/controller/init_v_controll_logic/Calculator.py
+++ init-v/controller/init_v_controll_logic/Calculator.py
@@ -1,7 +1,24 @@
+from controller.init_v_controll_logic import BackendAdapter
+from model import Configuration, RunResult
+from model.network import NetworkTopology,Connection,Device
+
+
 class Calculator:
-    def calculate_topology(self, PCAP_path: str, config: Configuration) -> NetworkTopology:
-        #TODO implement
-        pass
-    def calculate_run(self, PCAP_path: str, config: Configuration) -> RunResult:
-        # TODO implement
-        pass
+
+    def __init__(self):
+        self.backend_adapter = BackendAdapter()
+
+    def calculate_topology(self, pcap_path: str) -> NetworkTopology:
+        macs,ips,connections = self.backend_adapter.get_topology_information(pcap_path)
+        # TODO implementation details
+
+    def calculate_run(self, pcap_path: str, config: Configuration) -> (RunResult, RunResult):
+        pca_result: RunResult
+        autoencoder_result: RunResult
+        if config.autoencoder:
+            autoencoder_history, autoencoder_packet_mapping = self.backend_adapter.calculate_autoencoder(pcap_path,config)
+            # TODO creation of RunResult
+        if config.pca:
+            pca_performance, pca_packet_mapping = self.backend_adapter.calculate_pca(pcap_path,config)
+            # TODO creation of RunResult
+        return pca_result,autoencoder_result
