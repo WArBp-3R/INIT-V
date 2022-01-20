058cdd7bbefe1bab895c64e1c11efb6311aa9b0e
Deniz İmge Avcı <denizimgeavci@gmail.com>
2022-01-20 14:23:59 +0100

    Started working on calculator methods, created first version of Calculator.calculate_topology method.

--- init-v/controller/init_v_controll_logic/BackendAdapter.py
+++ init-v/controller/init_v_controll_logic/BackendAdapter.py
@@ -6,21 +6,18 @@ from keras.callbacks import History
 
 class BackendAdapter(BackendInterface):
 
-    def __init__(self):
-        self.backend = Backend()
-        self.pcap_id = ""
-        self.pcap_path = ""
+    def __init__(self, pcap_path: str):
+        self.backend = Backend.Backend()
+        self.pcap_id: str = self.backend.set_pcap(pcap_path)
 
-    def calculate_pca(self, pcap_path: str, config: Configuration) -> ((float, float), list):
-        self._update_pcap(pcap_path)
+    def calculate_pca(self, config: Configuration) -> ((float, float), list):
         self._configure_preprocessor(config)
         self.backend.set_parameters_pca(config.length_scaling)
         performance = self.backend.train_pca(self.pcap_id)
         packets = self.backend.encode_pca(self.pcap_id)
         return performance, packets
 
-    def calculate_autoencoder(self, pcap_path: str, config: Configuration) -> (History, list):
-        self._update_pcap(pcap_path)
+    def calculate_autoencoder(self, config: Configuration) -> (History, list):
         self._configure_preprocessor(config)
         autoencoder_config: AutoencoderConfiguration = config.autoencoder_config
         self.backend.set_parameters_autoencoder(number_of_hidden_layers=autoencoder_config.number_of_layers,
@@ -32,14 +29,17 @@ class BackendAdapter(BackendInterface):
         packets: list = self.backend.encode_autoencoder(self.pcap_id)
         return hist, packets
 
-    def get_topology_information(self, pcap_path: str) -> (list, list, dict):
-        self._update_pcap(pcap_path)
-        return self.backend.get_macs(self.pcap_id), self.backend.get_ips(self.pcap_id), self.backend.get_connections(self.pcap_id)
+    def get_device_macs(self) -> list:
+        return self.backend.get_macs(self.pcap_id)
 
-    def _update_pcap(self, pcap_path: str):
-        if self.pcap_path != pcap_path:
-            self.pcap_id = self.backend.set_pcap(pcap_path)
-            self.pcap_path = pcap_path
+    def get_associated_ips(self, mac_address: str) -> list:
+        return self.backend.get_ips(self.pcap_id, mac_address)
+
+    def get_connections(self) -> dict:
+        return self.backend.get_connections(self.pcap_id)
+
+    def update_pcap(self, pcap_path: str):
+        self.pcap_id = self.backend.set_pcap(pcap_path)
 
     def _configure_preprocessor(self, config: Configuration):
         self.backend.set_preprocessing(normalization_method=config.normalization,
--- init-v/controller/init_v_controll_logic/Calculator.py
+++ init-v/controller/init_v_controll_logic/Calculator.py
@@ -1,24 +1,46 @@
 from controller.init_v_controll_logic import BackendAdapter
-from model import Configuration, RunResult
+from model import Configuration, RunResult,
 from model.network import NetworkTopology,Connection,Device
-
-
 class Calculator:
 
-    def __init__(self):
-        self.backend_adapter = BackendAdapter()
+    def __init__(self, pcap_path: str):
+        self.backend_adapter = BackendAdapter.BackendAdapter(pcap_path)
 
-    def calculate_topology(self, pcap_path: str) -> NetworkTopology:
-        macs,ips,connections = self.backend_adapter.get_topology_information(pcap_path)
-        # TODO implementation details
+    def calculate_topology(self) -> NetworkTopology:
+        device_dict: dict[str,Device.Device] = dict()
+        connections: dict[str, Connection.Connection] = dict()
+        device_macs = self.backend_adapter.get_device_macs()
+        for x in device_macs:
+            device_ips = list()
+            ip_addresses: list = self.backend_adapter.get_associated_ips(x)
+            for y in ip_addresses:
+                device_ips.append(y)
+            device_dict.update({x : Device.Device(x, device_ips)})
+        all_connections= self.backend_adapter.get_connections()
+        for device in device_dict:
+            connections: dict = all_connections.get(device)
+            for protocol in connections:
+                for connected_device in connections.get(protocol):
+                    if connected_device not in all_connections:
+                        connections.update({device : Connection.Connection(device_dict.get(device), device_dict.get(connected_device))})
+                        # TODO add protocol of device as well
+        return NetworkTopology.NetworkTopology(list(device_dict.values()), list(connections.values()))
+
+
+
+
+
 
     def calculate_run(self, pcap_path: str, config: Configuration) -> (RunResult, RunResult):
         pca_result: RunResult
         autoencoder_result: RunResult
         if config.autoencoder:
-            autoencoder_history, autoencoder_packet_mapping = self.backend_adapter.calculate_autoencoder(pcap_path,config)
+            autoencoder_history, autoencoder_packet_mapping \
+                = self.backend_adapter.calculate_autoencoder(config)
             # TODO creation of RunResult
         if config.pca:
-            pca_performance, pca_packet_mapping = self.backend_adapter.calculate_pca(pcap_path,config)
+            pca_performance, pca_packet_mapping = self.backend_adapter.calculate_pca(config)
             # TODO creation of RunResult
-        return pca_result,autoencoder_result
+        return pca_result, autoencoder_result
+
+
