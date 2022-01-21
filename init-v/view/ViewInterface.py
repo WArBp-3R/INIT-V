7ebb587be9d1d68f501173768ca00fd66197eb0c
Johannes Heid <uyowy@student.kit.edu>
2022-01-21 12:48:53 +0100

    Changed view interface and changed the implementation in the Session class. Added data types to methods.

--- init-v/view/ViewInterface.py
+++ init-v/view/ViewInterface.py
@@ -1,53 +1,64 @@
 from model.network.NetworkTopology import NetworkTopology
 from model import Configuration
 from controller.init_v_controll_logic import ExportOptions
+from keras.callbacks import History
+from model.IStatistic import IStatistic
 
 
 class ViewInterface:
 
     def create_view(self):
         pass
-    def update_performance(self, pca: list, autoencoder: History):
+
+    def update_performance(self, pca: list[(float, float)], autoencoder: History):
         pass
-    def update_methods(self, pca_result: list, autoencoder_result: list):
+
+    def update_methods(self, pca_result: list[(float, float)], autoencoder_result: list[(float, float)]):
         pass
-    def update_topology(self, topolgy: NetworkTopology):
+
+    def update_topology(self, topology: NetworkTopology):
         pass
-    def update_statistics(self, stats : list):
+
+    def update_statistics(self, stats: list[IStatistic]):
         pass
+
     def update_configuration(self, config: Configuration):
         pass
-    def get_run_list(self)->list:
+
+    def get_run_list(self) -> list:
         pass
+
     def create_run(self):
         pass
-    def update_compare_performance(self, pca: list, autoencoder: History, pos:list):
+
+    def update_compare_performance(self, pca_performances: list[list[(float, float)]],
+                                   autoencoder_performances: list[History], timestamps: list[int]):
         pass
-    def update_compare_methods(self, pca_result: list, autoencoder_result: list, pos: list):
+
+    def update_compare_methods(self, pca_results: list[list[(float, float)]],
+                               autoencoder_results: list[list[(float, float)]], timestamps: list[int]):
         pass
-    def update_compare_statistics(self, stats : list, pos: list):
+
+    def update_compare_statistics(self, stats: list[list[IStatistic]], timestamps: list[int]):
         pass
-    def update_compare_configuration(self, config: Configuration, pos: list):
+
+    def update_compare_configuration(self, configs: list[Configuration], timestamps: list[int]):
         pass
 
     def compare_runs(self, pos: list):
         pass
+
     def load_session(self, source_path: str):
         pass
+
     def load_config(self, source_path: str):
         pass
+
     def save_session(self, output_path: str):
         pass
-    def save_sonfig(self, output_path: str):
+
+    def save_config(self, output_path: str):
         pass
+
     def export(self, output_path: str, options: ExportOptions):
         pass
-
-
-
-
-
-
-
-
-
