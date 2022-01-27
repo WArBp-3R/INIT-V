15a7b7d09f27f71662c0f648b331be6a83822bb1
thorben.comes <thorben.comes@scriptic.de>
2022-01-27 14:39:59 +0100

    updated ViewInterface.py methods and signatures

--- init-v/view/ViewInterface.py
+++ init-v/view/ViewInterface.py
@@ -11,39 +11,10 @@ class ViewInterface:
     def create_view(self, communicator):
         pass
 
-    def update_performance(self, pca: list[(float, float)], autoencoder: History):
-        pass
-
-    def update_methods(self, pca_result: list[(float, float)], autoencoder_result: list[(float, float)]):
-        pass
-
-    def update_topology(self, topology: NetworkTopology):
-        pass
-
-    def update_statistics(self, stats: list[IStatistic]):
-        pass
-
-    def update_configuration(self, config: Configuration):
-        pass
-
     def get_run_list(self) -> list:
         pass
 
     def create_run(self, lsc, vsc, nrm, mtd, hly, nhl, lsf, epc, opt):
-        pass
-
-    def update_compare_performance(self, pca_performances: list[list[(float, float)]],
-                                   autoencoder_performances: list[History], timestamps: list[datetime]):
-        pass
-
-    def update_compare_methods(self, pca_results: list[list[(float, float)]],
-                               autoencoder_results: list[list[(float, float)]], timestamps: list[datetime]):
-        pass
-
-    def update_compare_statistics(self, stats: list[list[IStatistic]], timestamps: list[datetime]):
-        pass
-
-    def update_compare_configuration(self, configs: list[Configuration], timestamps: list[datetime]):
         pass
 
     def compare_runs(self, pos: list):
@@ -55,10 +26,10 @@ class ViewInterface:
     def load_config(self, source_path: str):
         pass
 
-    def save_session(self, output_path: str):
+    def save_session(self, output_path: str, lsc, vsc, nrm, mtd, hly, nhl, lsf, epc, opt):
         pass
 
-    def save_config(self, output_path: str):
+    def save_config(self, output_path: str, lsc, vsc, nrm, mtd, hly, nhl, lsf, epc, opt):
         pass
 
     def export(self, output_path: str, options: ExportOptions):
