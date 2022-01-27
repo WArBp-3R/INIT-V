743843df79c4abded10b68e363f5c1b6b1edac51
WArBp-3R <walex.boettcher@pm.me>
2022-01-27 12:39:49 +0100

    Temporarily implemented dash app in GUI_Handler.py

--- init-v/view/GUI_Handler.py
+++ init-v/view/GUI_Handler.py
@@ -1,34 +1,63 @@
+import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Output, Input
 
-from .gui_creator.ComparePanelCreator import ComparePanelCreator
-from .gui_creator.DashboardPanelCreator import DashboardPanelCreator
+app = dash.Dash(__name__)
+app.config.suppress_callback_exceptions = True
+
+
+def get_input_id():
+    ctx = dash.callback_context
+    return ctx.triggered[0]['prop_id'].split('.')[0] if ctx.triggered else None
 
 
 class GUIHandler:
     def __init__(self):
+        # import main panel creators
+        from .gui_creator.ComparePanelCreator import ComparePanelCreator
+        from .gui_creator.DashboardPanelCreator import DashboardPanelCreator
+        from view.gui_creator.PanelCreator import PanelCreator
+
         dashboard_panel_creator = DashboardPanelCreator()
         compare_panel_creator = ComparePanelCreator()
 
-        self.panel_creators = {
-            dashboard_panel_creator.panel.desc_prefix: dashboard_panel_creator,
-            compare_panel_creator.panel.desc_prefix: compare_panel_creator
-        }
+        self.panel_creators = {}
+        self.generate_panel_creators([dashboard_panel_creator, compare_panel_creator])
+        self.default_panel_creator: PanelCreator = self.panel_creators[dashboard_panel_creator.panel.desc_prefix]
 
-        for pc in self.panel_creators.values():
-            sub_panel_creators = pc.sub_panel_creators
-            for spc in sub_panel_creators:
-                self.panel_creators[spc.panel.desc_prefix] = spc
+        self.url = dcc.Location(id="url")
+        self.window = html.Div(id="window")
 
-        self.url = dcc.Location(id='url')
-        self.window = dcc.Location(id="window")
-        self.default_panel = self.panel_creators["dashboard"].generate_content()
+        app.layout = self.get_layout()
+
+        app.callback(
+            Output("window", "children"),
+            Input("url", "pathname")
+        )(self.display_page)
+
+        app.run_server(debug=True)
+
+    def generate_panel_creators(self, panel_creators):
+        from view.gui_creator.PanelCreator import PanelCreator
+        sub_panel_creators: dict[str, PanelCreator] = {}
+        for pc in panel_creators:
+            sub_panel_creators[pc.panel.desc_prefix] = pc
+        for spc in sub_panel_creators.values():
+            self.generate_panel_creators(spc.sub_panel_creators.values())
+        self.panel_creators.update(sub_panel_creators)
 
     def get_layout(self):
-        html.Div(id="app", children=[
-            self.url,
-            html.Div(id="window", children=[self.default_panel])])
+        self.default_panel_creator.generate_content()
+        return html.Div(id="app", children=[self.url, self.window])
 
-    # TODO - callback
-    def display_page(self, path):
-        pass
+    def display_page(self, path):  # callback
+        path_str = str(path)[1:]
+        print(path_str)  # dbg
+        # print(self.panel_creators)
+        if path_str in self.panel_creators:
+            self.panel_creators[path_str].generate_content()
+            return [self.panel_creators[path_str].panel.layout]
+        else:
+            self.default_panel_creator.generate_content()
+            return [self.default_panel_creator.panel.layout]
