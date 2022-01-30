43ae78bb06c7854ab61fbc28d77d60591443b35c
thorben.comes <thorben.comes@scriptic.de>
2022-01-30 13:10:36 +0100

    implemented ConfigDecoder_test.py, last commit: also compareClass and randCreator

--- init-v-tests/controller/file_manager/ConfigDecoder_test.py
+++ init-v-tests/controller/file_manager/ConfigDecoder_test.py
@@ -1,13 +1,18 @@
-import random
+import RandCreator
+import CompareClass
 
 from model.Configuration import Configuration
 from model.AutoencoderConfiguration import AutoencoderConfiguration
 from controller.file_manager.ConfigDecoder import ConfigDecoder
 
+test_path = ''
+#TODO add test path
+
 def test_load_configuration():
-
-    c = Configuration()
+    filename = 'load_config_test_file_name_variable_value'
+    c = RandCreator.create_rand_config()
     #TODO save config
-
+    loaded = ConfigDecoder.load_configuration(test_path + filename)
+    assert(CompareClass.configuration_equal(c, loaded))
 
 
