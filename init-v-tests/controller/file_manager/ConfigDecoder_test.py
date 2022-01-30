09ee06f66a562f01b604c0c8802943dc27111a8b
thorben.comes <thorben.comes@scriptic.de>
2022-01-30 13:22:01 +0100

    wrote config de- and en-coder test classes

--- init-v-tests/controller/file_manager/ConfigDecoder_test.py
+++ init-v-tests/controller/file_manager/ConfigDecoder_test.py
@@ -1,18 +1,16 @@
 import RandCreator
 import CompareClass
-
-from model.Configuration import Configuration
-from model.AutoencoderConfiguration import AutoencoderConfiguration
+from controller.file_manager.ConfigEncoder import ConfigEncoder
 from controller.file_manager.ConfigDecoder import ConfigDecoder
 
 test_path = ''
-#TODO add test path
+
+
+# TODO add test path, change filename
 
 def test_load_configuration():
     filename = 'load_config_test_file_name_variable_value'
     c = RandCreator.create_rand_config()
-    #TODO save config
+    ConfigEncoder.save(test_path+filename, c)
     loaded = ConfigDecoder.load_configuration(test_path + filename)
     assert(CompareClass.configuration_equal(c, loaded))
-
-
--- init-v-tests/controller/file_manager/ConfigEncoder_test.py
+++ init-v-tests/controller/file_manager/ConfigEncoder_test.py
@@ -1,22 +1,16 @@
-import csv
+import RandCreator
+import CompareClass
+from controller.file_manager.ConfigDecoder import ConfigDecoder
+from controller.file_manager.ConfigEncoder import ConfigEncoder
 
-from model.Configuration import Configuration
-from model.AutoencoderConfiguration import AutoencoderConfiguration
-
-header = ['autoencoder', 'pca', 'length_scaling', 'normalization', 'number_of_layers', 'number_of_nodes',
-          'loss_function', 'number_of_epochs', 'optimizer']
+test_path = ''
 
 
-class ConfigEncoder:
-    def save(self, output_path: str, config: Configuration):
-        # TODO Test
+# TODO add test path, change filename
 
-        data = [config.autoencoder, config.pca, config.length_scaling, config.normalization, config.autoencoder_config.number_of_layers,
-                config.autoencoder_config.number_of_nodes, config.autoencoder_config.loss_function, config.autoencoder_config.number_of_epochs, config.autoencoder_config.optimizer]
-
-        with open(output_path, mode = 'w', encoding='UTF8', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(header)
-            writer.writerow(data)
-
-        pass
+def test_save():
+    filename = 'load_config_test_file_name_variable_value'
+    c = RandCreator.create_rand_config()
+    ConfigEncoder.save(test_path + filename, c)
+    loaded = ConfigDecoder.load_configuration(test_path + filename)
+    assert (CompareClass.configuration_equal(c, loaded))
