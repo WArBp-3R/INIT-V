b59782f346ce14fdfce109a166a92fae7602aca9
Johannes Heid <uyowy@student.kit.edu>
2022-01-31 16:41:59 +0100

    Model test.

--- init-v-tests/model/SessionTest.py
+++ init-v-tests/model/SessionTest.py
@@ -1,10 +1,5 @@
-import unittest
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_constructor():
+    pass
+    # TODO
