84a1a484ea8bb5677491e2f64e909cd6a721cac6
WArBp-3R <walex.boettcher@pm.me>
2022-02-06 18:49:27 +0100

    Fixed update_network callback missing in MethodResultsPanelCreator.py

--- init-v/view/gui_creator/MethodResultsPanelCreator.py
+++ init-v/view/gui_creator/MethodResultsPanelCreator.py
@@ -27,6 +27,12 @@ class MethodResultsPanelCreator(PanelCreator):
             Input(self.panel.get_menu()["merge"].id, "n_clicks")
         )(self.toggle_method_results_graphs)
 
+        app.callback(
+            Output(self.panel.format_specifier("active_protocols"), "options"),
+            Output(self.panel.get_menu()["protocols"].dropdown.id, "style"),
+            Input(self.panel.get_menu()["protocols"].btn.id, "n_clicks"),
+        )(self.update_protocols)
+
     def generate_menu(self):
         m_res_menu = self.panel.get_menu()
         m_res_menu.add_menu_item("merge", "Merge")
@@ -48,13 +54,7 @@ class MethodResultsPanelCreator(PanelCreator):
         self.graph_style_outputs = [Output(g, "style") for g in graph_ids]
 
         # TODO - get protocols from view interface(?)
-        self.active_protocols = dcc.Checklist(id=self.panel.format_specifier("active_protocols"),
-                                              options=[
-                                                  {"label": "protocol placeholder1", "value": "P"},
-                                                  {"label": "TCP", "value": "TCP"},
-                                                  {"label": "PROFINET", "value": "PROFINET"}
-                                              ],
-                                              value=[])
+        self.active_protocols = dcc.Checklist(id=self.panel.format_specifier("active_protocols"))
 
         protocol_list_content = self.panel.get_menu()["protocols"].dropdown.set_content()
         protocol_list_content.components = [self.active_protocols]
@@ -68,3 +68,19 @@ class MethodResultsPanelCreator(PanelCreator):
             return disabled, disabled, enabled
         else:
             return enabled, enabled, disabled
+
+    # TODO - replace stub (WIP)
+    def update_protocols(self, btn):
+        button_id = get_input_id()
+        print("update_protocols")
+        # view adapter stuff
+        protocol_options = [{"label": "protocol placeholder1", "value": "P"},
+                            {"label": "TCP", "value": "TCP"},
+                            {"label": "PROFINET", "value": "PROFINET"}, ]
+        style_result = {"display": "none"}
+        if button_id == self.panel.get_menu()["protocols"].btn.id:
+            if btn % 2 == 1:
+                style_result = {"display": "flex"}
+        else:
+            pass
+        return protocol_options, style_result
