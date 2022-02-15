a3a9314abeb08e94ba464c163de833be12900056
WArBp-3R <walex.boettcher@pm.me>
2022-02-15 16:10:30 +0100

    correctly cast pca performance data

--- init-v/controller/init_v_controll_logic/BackendAdapter.py
+++ init-v/controller/init_v_controll_logic/BackendAdapter.py
@@ -17,8 +17,9 @@ class BackendAdapter(BackendInterface):
         self._configure_preprocessor(config)
         self.backend.set_parameters_pca(config.length_scaling)
         performance = self.backend.train_pca(self.pcap_id)
+        float_perf = (float(performance[0]), float(performance[1]))
         packets = self.backend.encode_pca(self.pcap_id)
-        return performance, packets
+        return float_perf, packets
 
     def calculate_autoencoder(self, config: Configuration) -> (History, list):
         self._configure_preprocessor(config)
