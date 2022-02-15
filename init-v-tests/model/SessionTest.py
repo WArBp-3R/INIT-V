a58ef3e66d0ddbe205659c21f6fee2ffc3d8f36c
Johannes Heid <uyowy@student.kit.edu>
2022-02-15 14:04:09 +0100

    test session methods

--- init-v-tests/model/SessionTest.py
+++ init-v-tests/model/SessionTest.py
@@ -6,7 +6,7 @@ from model.Session import Session
 import CompareClass
 
 
-def test_constructor():
+def test():
     PCAP_PATH: str = "packets.pcapng"
     protocols: set[str] = {"TCP", "UDP"}
     run_results: list[RunResult] = []
@@ -23,7 +23,15 @@ def test_constructor():
     while i < 5:
         assert CompareClass.run_result_equal(session.run_results[i], run_results[i])
     assert CompareClass.configuration_equal(active_config, session.active_config)
-    
+
+    rr: RunResult = RandCreator.create_rand_run_result(10)
+    session.add_run_result(rr)
+    assert CompareClass.run_result_equal(rr, session.run_results[-1])
+
+    n_config: Configuration = RandCreator.create_rand_config()
+    session.update_configuration(n_config)
+    assert CompareClass.configuration_equal(n_config, session.active_config)
+
 
 
 
