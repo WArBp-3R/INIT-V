5c2fe2527139b2ae71374afc7f2149fcb4d03067
Deniz İmge Avcı <denizimgeavci@gmail.com>
2022-02-25 10:59:28 +0100

    Modified MethodResultContainer and its usage in a more clean way in DashboardPanelCreator.py

--- init-v/view/utility/MethodResultContainer.py
+++ init-v/view/utility/MethodResultContainer.py
@@ -10,11 +10,12 @@ class MethodResultContainer:
         self.packet_figure_y = [item[1] for item in mappings]
         self.packet_figure_dict = dict({"x": self.packet_figure_x, "y": self.packet_figure_y,
                                         "protocols": self.protocols})
-        for hover_item in hover_data[0].keys():
-            self.packet_figure_dict[hover_item] = [hover_information[hover_item] for hover_information in hover_data]
-
-        self.figure = px.scatter(self.packet_figure_dict, x="x", y="y", color="protocols",
-                                 hover_data=hover_data[0].keys())
+        self.figure = None
+        if len(hover_data) > 0:
+            for hover_item in hover_data[0].keys():
+                self.packet_figure_dict[hover_item] = [hover_information[hover_item] for hover_information in hover_data]
+            self.figure = px.scatter(self.packet_figure_dict, x="x", y="y", color="protocols",
+                                     hover_data=hover_data[0].keys())
 
 
 def merge_result_containers(results: list[MethodResultContainer]) -> MethodResultContainer:
@@ -22,8 +23,7 @@ def merge_result_containers(results: list[MethodResultContainer]) -> MethodResul
     all_packet_protocols = list()
     all_packet_hover_data = list()
     for result in results:
-        if result is not None:
-            all_packet_mappings += result.mappings
-            all_packet_protocols += result.protocols
-            all_packet_hover_data += result.hover_data
+        all_packet_mappings += result.mappings
+        all_packet_protocols += result.protocols
+        all_packet_hover_data += result.hover_data
     return MethodResultContainer(all_packet_mappings, all_packet_protocols, all_packet_hover_data)
