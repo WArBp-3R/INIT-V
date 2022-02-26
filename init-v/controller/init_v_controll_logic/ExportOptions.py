19536045bc143ea92ae28b99ab7cbbcdcbd7ced5
Deniz İmge Avcı <denizimgeavci@gmail.com>
2022-02-26 17:06:55 +0100

    Changed name of type to export_type for clarity purposes

--- init-v/controller/init_v_controll_logic/ExportOptions.py
+++ init-v/controller/init_v_controll_logic/ExportOptions.py
@@ -1,7 +1,7 @@
 class ExportOptions:
 
-    def __init__(self, type: str, resolution: (int, int), introduction: bool, autoencoder_result: bool, pca_result: bool, autoencoder_performance: bool, pca_performance: bool, topology: bool, statistics: bool):
-        self.type = type
+    def __init__(self, export_type: str, resolution: (int, int), introduction: bool, autoencoder_result: bool, pca_result: bool, autoencoder_performance: bool, pca_performance: bool, topology: bool, statistics: bool):
+        self.export_type = export_type
         self.resolution = resolution
         self.introduction = introduction
         self.autoencoder_result = autoencoder_result
