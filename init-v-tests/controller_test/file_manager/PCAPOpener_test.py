fce7b8e69de90ba0ca62dd87777e1f64baa48dfb
thorben.comes <thorben.comes@scriptic.de>
2022-03-03 13:24:04 +0100

    wrote PCAPOpener_test.py and modified method signature in PCAPOpener.py

--- init-v-tests/controller_test/file_manager/PCAPOpener_test.py
+++ init-v-tests/controller_test/file_manager/PCAPOpener_test.py
@@ -1,7 +1,13 @@
-from model.Session import Session
+from controller.file_manager.PCAPOpener import PCAPOpener
+import RandCreator
+import CompareClass
+
+test_pcap_path = ''
+config = RandCreator.create_rand_config()
 
 
-class PCAPOpener:
-    def load_pcap(self, source_path: str) -> Session:
-        #TODO implement
-        pass
+def test_load_pcap():
+    pc = PCAPOpener()
+    session = pc.load_pcap(test_pcap_path, config)
+    s_config = session.active_config
+    assert(CompareClass.configuration_equal(config, s_config))
--- init-v/controller/file_manager/PCAPOpener.py
+++ init-v/controller/file_manager/PCAPOpener.py
@@ -1,9 +1,12 @@
+from model.Session import Session
+from model.Configuration import Configuration
+
 class PCAPOpener:
     """
     method to open  a new session with a .pcapng file.
 
     :param source_path: string containing the path to the .pcapng
     """
-    def load_pcap(self, source_path: str) -> Session:
+    def load_pcap(self, source_path: str, config: Configuration) -> Session:
         #TODO implement
         pass
