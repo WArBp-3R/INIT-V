6671d6f0d56ac1099ce4bfbab0bbaa2c89325ed7
thorben.comes <thorben.comes@scriptic.de>
2022-03-04 11:49:17 +0100

    created stubs for tests of yet to be implemented classes

--- init-v-tests/controller_test/file_manager/ExportCreator_test.py
+++ init-v-tests/controller_test/file_manager/ExportCreator_test.py
@@ -1,8 +1,2 @@
-from model.Configuration import Configuration
-from model.Session import Session
-from controller.init_v_controll_logic.ExportOptions import ExportOptions
-
-class ExportCreator:
-    def export(self, output_path: str, session: Session, options: ExportOptions):
-        #TODO implement
-        pass
+def test_export():
+    pass
--- init-v-tests/controller_test/file_manager/ImageExporter_test.py
+++ init-v-tests/controller_test/file_manager/ImageExporter_test.py
@@ -1,8 +1,2 @@
-from model.Session import Session
-from controller.init_v_controll_logic.ExportOptions import ExportOptions
-
-
-class ImageExporter:
-    def export_image(self, output_path: str, session: Session, options: ExportOptions):
-        #TODO implement
-        pass
+def test_img_export():
+    pass
