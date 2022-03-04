0c6f802ec4385f135b82c62efce2552213e4b152
thorben.comes <thorben.comes@scriptic.de>
2022-03-04 13:06:10 +0100

    fixed CompareClass.py

--- init-v-tests/CompareClass.py
+++ init-v-tests/CompareClass.py
@@ -9,11 +9,13 @@ from model.network.Connection import Connection
 
 
 def configuration_equal(c1: Configuration, c2: Configuration) -> bool:
+
     a1 = c1.autoencoder_config
     a2 = c2.autoencoder_config
     p1 = (c1.pca == c2.pca) and (c1.normalization == c2.normalization) and (c1.sample_size == c2.sample_size)
     p2 = (c1.autoencoder == c2.autoencoder) and (a1.optimizer == a2.optimizer)
-    p3 = (a1.number_of_nodes == a2.number_of_nodes) and (a1.number_of_layers == a2.number_of_layers)
+    p3 = (a1.nodes_of_hidden_layers == a2.nodes_of_hidden_layers) and \
+         (a1.number_of_hidden_layers == a2.number_of_hidden_layers)
     p4 = (a1.number_of_epochs == a2.number_of_epochs) and (a1.loss_function == a2.loss_function)
 
     return p1 and p2 and p3 and p4
