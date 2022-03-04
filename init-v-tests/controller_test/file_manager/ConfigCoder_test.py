62390b6ecfa1add43bcc898b71fa90289150cfba
thorben.comes <thorben.comes@scriptic.de>
2022-03-04 13:06:31 +0100

    fixed ConfigCoder_test.py

--- init-v-tests/controller_test/file_manager/ConfigCoder_test.py
+++ init-v-tests/controller_test/file_manager/ConfigCoder_test.py
@@ -5,14 +5,16 @@ import CompareClass
 from controller.file_manager.ConfigEncoder import ConfigEncoder
 from controller.file_manager.ConfigDecoder import ConfigDecoder
 
-test_path = os.getcwd()
-
+test_path = os.getcwd() + os.sep
+ce = ConfigEncoder()
+cd = ConfigDecoder()
 
 # TODO add test path, change filename
+
 
 def test_load_configuration():
     filename = '' + os.sep + 'load_config_test_file_name_variable_value.csv'
     c = RandCreator.create_rand_config()
-    ConfigEncoder.save(test_path+filename, c)
-    loaded = ConfigDecoder.load_configuration(test_path + filename)
+    ce.save(test_path+filename, c)
+    loaded = cd.load_configuration(test_path + filename)
     assert(CompareClass.configuration_equal(c, loaded))
