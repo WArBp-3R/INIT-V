a47ac3920af247831142b180210034be310ebf3d
thorben.comes <thorben.comes@scriptic.de>
2022-03-06 19:33:35 +0100

    wrote test for session en- and decoder

--- init-v-tests/controller_test/file_manager/SessionCoder_test.py
+++ init-v-tests/controller_test/file_manager/SessionCoder_test.py
@@ -1,14 +1,30 @@
 import os
+import sys
+import shutil
 
 from controller.file_manager.SessionDecoder import SessionDecoder
 from controller.file_manager.SessionEncoder import SessionEncoder
 import CompareClass
 import RandCreator
 
-test_path = os.getcwd()
+try:
+    os.mkdir(os.getcwd() + os.sep + 'test_dir')
+except OSError:
+    # TODO add error handling
+    pass
+
+test_path = os.getcwd() + os.sep + 'test_dir' + os.sep
+
+se = SessionEncoder()
+sd = SessionDecoder()
+
 
 def test_session_coder():
     saved = RandCreator.create_rand_session(0, 0, 0, 0, 0)
-    SessionEncoder.save(test_path + os.sep + 'test_dir' + os.sep + 'test_session', saved, None)
-    loaded = SessionDecoder.load_session(test_path + os.sep + 'test_dir' + os.sep + 'test_session')
+    try:
+        shutil.rmtree(test_path + 'test_session')
+    except OSError as e:
+        print("Error: %s - %s." % (e.filename, e.strerror))
+    se.save(test_path + 'test_session', saved, None)
+    loaded = sd.load_session(test_path + 'test_session')
     assert CompareClass.session_equal(saved, loaded)
