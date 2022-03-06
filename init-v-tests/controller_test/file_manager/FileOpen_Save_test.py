52af127e1c08c82741e9b7a877218f89cda7814e
thorben.comes <thorben.comes@scriptic.de>
2022-03-06 20:18:58 +0100

    updated FileOpen_Save_test.py

--- init-v-tests/controller_test/file_manager/FileOpen_Save_test.py
+++ init-v-tests/controller_test/file_manager/FileOpen_Save_test.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 from controller.file_manager.FileOpener import FileOpener
 from controller.file_manager.FileSaver import FileSaver
@@ -6,7 +7,7 @@ from controller.file_manager.FileSaver import FileSaver
 import RandCreator
 import CompareClass
 
-test_path = os.getcwd()
+test_path = os.getcwd() + os.sep + 'test_dir'
 fs = FileSaver()
 fo = FileOpener()
 
@@ -14,6 +15,10 @@ fo = FileOpener()
 def test_open_save():
     saved_config = RandCreator.create_rand_config()
     saved_session = RandCreator.create_rand_session(0, 0, 0, 0, 0)
+    try:
+        shutil.rmtree(test_path + os.sep + 'session_FOS')
+    except OSError as e:
+        print("Error: %s - %s." % (e.filename, e.strerror))
     fs.save(test_path + os.sep + 'test_config_FOS.csv', saved_config)
     fs.save(test_path + os.sep + 'session_FOS', saved_session, None)
     loaded_config = fo.load(test_path + os.sep + 'test_config_FOS.csv', 'c')
