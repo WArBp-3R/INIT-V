0603b49f118381ff91e782d7797a050287f44d74
thorben.comes <thorben.comes@scriptic.de>
2022-03-14 11:11:27 +0100

    wrote logs for SessionEncoder.py

--- init-v/controller/file_manager/SessionEncoder.py
+++ init-v/controller/file_manager/SessionEncoder.py
@@ -7,6 +7,8 @@ import dash_cytoscape as cyto
 from controller.file_manager.ConfigEncoder import ConfigEncoder
 from model.Session import Session
 
+import logging
+
 
 class SessionEncoder:
 
@@ -17,11 +19,11 @@ class SessionEncoder:
         :param  output_path: string containing the path to the output (path || name).
         :param session: Session object to be written to disk.
         """
-        # TODO implement
 
         try:
             os.makedirs(output_path)
-        except OSError:
+        except OSError as err:
+            logging.error('Session encoder error: ' + str(err))
             # TODO add error handling
             pass
 
@@ -37,19 +39,23 @@ class SessionEncoder:
         # saves topology in session folder
         with open(output_path + os.sep + "Topology", mode='wb') as topology:
             pickle.dump(session.topology, topology)
+        logging.debug('topology saved')
 
         # saves topology_graph in session folder
         with open(output_path + os.sep + "Topology_graph", mode='wb') as topology_g_file:
             pickle.dump(topology_graph, topology_g_file)
+        logging.debug('topology graph saved')
 
         with open(output_path + os.sep + "statistics", mode='wb') as stats_file:
             pickle.dump(session.statistics, stats_file)
+        logging.debug('statistics saved')
 
         # saves the list of protocols
         with open(output_path + os.sep + "list_of_protocols.csv", mode='w', encoding='UTF8', newline='') as file:
             writer = csv.writer(file)
             for i in session.protocols:
                 writer.writerow([i])
+        logging.debug('list of protocols saved')
 
         # saves the list of highest protocols
         with open(output_path + os.sep + "list_of_highest_protocols.csv", mode='w', encoding='UTF8',
@@ -57,6 +63,7 @@ class SessionEncoder:
             writer = csv.writer(file)
             for i in session.highest_protocols:
                 writer.writerow([i])
+        logging.debug('list of highest protocols saved')
 
         # saves runs in a directory
         for x in session.run_results:
@@ -66,10 +73,12 @@ class SessionEncoder:
             print(x.timestamp)
             try:
                 os.makedirs(run_path)
-            except OSError:
+            except OSError as err:
+                logging.error('Session encoder error: ' + str(err))
                 # TODO add error handling
                 pass
             # else:
             config_encoder.save(run_path + os.sep + "configuration.csv", x.config)
             with open(run_path + os.sep + "Run_Results", mode='wb') as run_file:
                 pickle.dump(x, run_file)
+        logging.debug('runs saved')
