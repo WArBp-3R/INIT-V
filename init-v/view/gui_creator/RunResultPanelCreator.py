2f97a30cbb3117cf2f66dca863884d774f96dff2
WArBp-3R <walex.boettcher@pm.me>
2022-03-18 12:22:35 +0100

    changed callback triggers for runs and run_list

--- init-v/view/gui_creator/RunResultPanelCreator.py
+++ init-v/view/gui_creator/RunResultPanelCreator.py
@@ -12,7 +12,6 @@ class RunResultPanelCreator(PanelCreator):
 
     def __init__(self, handler, desc_prefix="run"):
         self.select_run_list = None
-        self.run_ids = None
 
         spc = [x[0](handler, desc_prefix=x[1]) for x in
                [(MethodResultsPanelCreator, f"{desc_prefix}_pnl_m-res"),
@@ -22,13 +21,10 @@ class RunResultPanelCreator(PanelCreator):
 
     def generate_menu(self):
         run_result_menu = self.panel.get_menu()
-        run_select_dd = run_result_menu.add_menu_item("select-run", "Select Run").set_dropdown()
+        run_result_menu.add_menu_item("select-run", "Select Run").set_dropdown()
 
     def generate_content(self):
-        self.run_ids = dcc.Input(id=self.panel.format_specifier("run_ids"), type="hidden", value="")
-
-        self.panel.content.components = [self.run_ids] + [spc.panel.layout for spc in
-                                                          self.sub_panel_creators.values()]
+        self.panel.content.components = [spc.panel.layout for spc in self.sub_panel_creators.values()]
 
         self.select_run_list = dcc.RadioItems(id=self.panel.format_specifier("select_run_list"))
         self.panel.get_menu()["select-run"].dropdown.set_content().components = [self.select_run_list]
@@ -41,14 +37,14 @@ class RunResultPanelCreator(PanelCreator):
 
         self.handler.cb_mgr.register_callback(
             [Output(self.panel.titlebar.title.id, "children")],
-            Input(self.run_ids.id, "value"),
-            lambda x: [f"{self.TITLE} - Run: {self.handler.interface.get_run_list()[int(x)]}"],
+            Input(self.select_run_list.id, "value"),
+            lambda x: [f"{self.TITLE} - Run: {int(x)}"],
             default_outputs=[self.TITLE]
         )
 
         self.handler.cb_mgr.register_callback(
             m_res_spc.graph_outputs,
-            Input(self.run_ids.id, "value"),
+            Input(self.select_run_list.id, "value"),
             m_res_spc.update_method_results_panel,
             default_outputs=[{"layout": {"title": "Autoencoder",
                                          "xaxis": {"title": "ex"},
@@ -65,7 +61,7 @@ class RunResultPanelCreator(PanelCreator):
 
         self.handler.cb_mgr.register_callback(
             perf_spc.result_outputs,
-            Input(self.run_ids.id, "value"),
+            Input(self.select_run_list.id, "value"),
             perf_spc.update_performance_panel,
             default_outputs=[{"layout": {"title": "Autoencoder",
                                          "xaxis": {"title": "ex"},
@@ -77,20 +73,12 @@ class RunResultPanelCreator(PanelCreator):
                               html.P(f"Delta: {None}")]]
         )
 
-        self.register_dropdown_list_update_callback(self.select_run_list, "select-run", self.update_select_run_list)
-
-        self.handler.cb_mgr.register_callback(
-            [Output(self.run_ids.id, "value")],
-            Input(self.select_run_list.id, "value"),
-            lambda x: [x],
-            default_outputs=[""]
-        )
+        # self.register_dropdown_list_update_callback(self.select_run_list, "select-run", self.update_select_run_list)
 
     # CALLBACK METHODS
-    def update_select_run_list(self, btn):
+    def update_select_run_list(self, void):
         run_options = []
         run_names = self.handler.interface.get_run_list()
         for i in range(0, len(run_names)):
-            run_options.append({"label": run_names[i], "value": str(i)})
-        style_result = {"display": "flex"} if btn % 2 == 1 else {"display": "none"}
-        return [run_options, style_result]
+            run_options.append({"label": f"{str(i)}: {run_names[i]}", "value": str(i)})
+        return [run_options, run_options[-1]["value"]] if len(run_options) > 0 else None
