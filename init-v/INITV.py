bac654223b96cce018a3d2aa7c36b0dfc5ee6ad0
thorben.comes <thorben.comes@scriptic.de>
2022-03-20 12:27:11 +0100

    improved gitignore

--- init-v/INITV.py
+++ init-v/INITV.py
@@ -1,6 +1,5 @@
 import os
 import sys
-
 if __name__ == "__main__":
     sys.path.append(f"{os.path.dirname(__file__)}{os.sep}..{os.sep}code")
     from controller.init_v_controll_logic.Controller import Controller
